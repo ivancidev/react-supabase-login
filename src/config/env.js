c3db0952c0f71edebb11a66fe73fbd2ea1e1c84f
Herlan <herlanherbas@gmail.com>
2024-07-09 11:14:23 -0400

    create config envjs

---
+++ src/config/env.js
@@ -0,0 +1,5 @@
+export const URL = import.meta.env.VITE_SUPABASE_URL;
+export const API_KEY = import.meta.env.VITE_SUPABASE_KEY;
+
+export const AUTH_URL = import.meta.env.VITE_AUTH_URL;
+export const AUTH_ANON_KEY = import.meta.env.VITE_AUTH_ANON_KEY;
