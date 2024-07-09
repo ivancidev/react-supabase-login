6706c71e9419e722d02306a250f56615a473780b
Herlan <herlanherbas@gmail.com>
2024-07-09 11:12:47 -0400

    init react vite

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Login from "./pages/Login";
+import ListCountry from "./components/ListCountry";
+
+function App() {
+  return(
+    <Login/>
+    // <ListCountry/>
+  )
+}
+
+export default App;
