eec15aae716bed2ea9f7f27215eecc6bbd60b2bb
Herlan <herlanherbas@gmail.com>
2024-07-10 14:43:13 -0400

    update envjs

--- src/config/env.js
+++ src/config/env.js
@@ -1,5 +1,3 @@
-export const URL = import.meta.env.VITE_SUPABASE_URL;
-export const API_KEY = import.meta.env.VITE_SUPABASE_KEY;
+export const URL_SUPABASE = import.meta.env.VITE_SUPABASE_URL;
+export const API_KEY_SUPABASE = import.meta.env.VITE_SUPABASE_KEY;
 
-export const AUTH_URL = import.meta.env.VITE_AUTH_URL;
-export const AUTH_ANON_KEY = import.meta.env.VITE_AUTH_ANON_KEY;
